8370f0a281138f2e480a4a03347ba3b0545773a4
Filipe Ramos <42271005+fil1pe@users.noreply.github.com>
2018-10-29 16:44:18 -0300

    Create NaiveStringSearch.java

---
+++ string-search/src/main/java/pra/stringsearch/NaiveStringSearch.java
@@ -0,0 +1,14 @@
+package pra.stringsearch;
+
+import java.lang.StringBuffer;
+
+/**
+ * Naive strategy for finding substring
+ */
+public abstract class NaiveStringSearch extends StringSearchStrategy {
+
+    public static int find(StringBuffer content, String substr){
+        
+    }
+
+}
