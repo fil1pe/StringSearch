ab3c2eec0568b900589309bbf0cb796acbd73d69
Filipe Ramos <42271005+fil1pe@users.noreply.github.com>
2018-10-30 11:52:18 -0300

    Update NaiveStringSearch.java

--- string-search/src/main/java/pra/stringsearch/NaiveStringSearch.java
+++ string-search/src/main/java/pra/stringsearch/NaiveStringSearch.java
@@ -3,14 +3,18 @@ package pra.stringsearch;
 /**
  * Naive strategy for finding substring
  */
-public abstract class NaiveStringSearch extends StringSearchStrategy {
+public class NaiveStringSearch extends StringSearchStrategy {
 
-    public int find(String content, String substr, int begin){
+    public NaiveStringSearch(String pattern){
+        super(pattern);
+    }
+    
+    public int find(String content, int begin){
         int n = content.length();
-        int m = substr.length();
+        int m = pattern.length();
         for(int i=begin;i<n-m;i++){
             int j=0;
-            while(j<m && content.charAt(i+j) == substr.charAt(j)) j++;
+            while(j<m && content.charAt(i+j) == pattern.charAt(j)) j++;
             if(j == m) return i;
         }
         return NOT_FOUND;
