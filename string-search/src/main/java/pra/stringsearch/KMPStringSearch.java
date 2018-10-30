7bf078433c2c43b9e0dc3b45b98350da164ab44b
Filipe Ramos <42271005+fil1pe@users.noreply.github.com>
2018-10-30 11:50:29 -0300

    Update KMPStringSearch.java

--- string-search/src/main/java/pra/stringsearch/KMPStringSearch.java
+++ string-search/src/main/java/pra/stringsearch/KMPStringSearch.java
@@ -2,18 +2,23 @@ package pra.stringsearch;
 
 /**
  * Knuth–Morris–Prat strategy for finding substring
- * source: http://www.stoimen.com/blog/2012/04/09/computer-algorithms-morris-pratt-string-searching/
  */
-public abstract class RabinKarpStringSearch extends StringSearchStrategy {
+public class RabinKarpStringSearch extends StringSearchStrategy {
+    
+    private final int[] next;
+    
+    public RabinKarpStringSearch(String pattern){
+        super(pattern);
+        preprocessKMP();
+    }
 
-    public int find(String content, String substr, int begin){
+    public int find(String content, int begin){
         int n = content.length();
         int m = substr.length();
         int i = 0;
         int j = 0;
-        int[] T = preprocessKMP(substr);
         while(j < n){
-            while(i > -1 && substr.charAt(i) != content.charAt(j)) i = T[i];
+            while(i > -1 && substr.charAt(i) != content.charAt(j)) i = next[i];
             i++;
             j++;
             if(i >= m) return j - i;
@@ -21,16 +26,15 @@ public abstract class RabinKarpStringSearch extends StringSearchStrategy {
         return NOT_FOUND;
     }
     
-    private int[] preprocessKMP(String pattern){
-        int[] T = new int[pattern.length()+1];
+    private final void preprocessKMP(){
+        next = new int[pattern.length()+1];
         int i = 0;
-        int j = T[0] = -1;
+        int j = next[0] = -1;
         int m = pattern.length();
         while(i < m){
-            while(j > -1 && pattern.charAt(i) != pattern.charAt(j)) j = T[j];
-            T[++i] = ++j;
+            while(j > -1 && pattern.charAt(i) != pattern.charAt(j)) j = next[j];
+            next[++i] = ++j;
         }
-        return T;
     }
 
 }
