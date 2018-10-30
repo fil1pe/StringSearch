177531530a9f1863efdae75aef2a33233c04784b
Filipe Ramos <42271005+fil1pe@users.noreply.github.com>
2018-10-30 09:15:45 -0300

    Create RabinKarpStringSearch.java

---
+++ string-search/src/main/java/pra/stringsearch/RabinKarpStringSearch.java
@@ -0,0 +1,23 @@
+package pra.stringsearch;
+
+import java.lang.StringBuffer;
+import static Utils.hash;
+
+/**
+ * Rabin Karp strategy for finding substring
+ */
+public abstract class NaiveStringSearch extends StringSearchStrategy {
+
+    public int find(String content, String substr, int begin){
+        int hpattern = hash(substr);
+        int n = content.length();
+        int m = substr.length();
+        for(int i=begin;i<n-m;i++){
+            String s = content.substring(i, i+m);
+            int hs = hash(s);
+            if(hs == hpattern) if(substr.equals(s)) return i;
+        }
+        return NOT_FOUND;
+    }
+
+}
