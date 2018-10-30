8b3c6ccbacd7cafcb351d917cb94ad076ef45f09
Filipe Ramos <42271005+fil1pe@users.noreply.github.com>
2018-10-30 09:16:52 -0300

    Update StringSearchStrategy.java

--- string-search/src/main/java/pra/stringsearch/StringSearchStrategy.java
+++ string-search/src/main/java/pra/stringsearch/StringSearchStrategy.java
@@ -7,6 +7,6 @@ public abstract class StringSearchStrategy {
 
     private int final NOT_FOUND = -1;
 
-    public int find(java.lang.StringBuffer content, String substr, int begin);
+    public int find(String content, String substr, int begin);
 
 }
