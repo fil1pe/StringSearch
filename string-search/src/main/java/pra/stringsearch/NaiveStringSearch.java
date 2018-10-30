60552d3e81162bcaff70a397f9237b5d7701770f
Filipe Ramos <42271005+fil1pe@users.noreply.github.com>
2018-10-30 08:48:08 -0300

    Update NaiveStringSearch.java

--- string-search/src/main/java/pra/stringsearch/NaiveStringSearch.java
+++ string-search/src/main/java/pra/stringsearch/NaiveStringSearch.java
@@ -7,8 +7,15 @@ import java.lang.StringBuffer;
  */
 public abstract class NaiveStringSearch extends StringSearchStrategy {
 
-    public static int find(StringBuffer content, String substr){
-        
+    public int find(StringBuffer content, String substr, int begin){
+        int n = content.length();
+        int m = substr.length();
+        for(int i=begin;i<n-m;i++){
+            int j=0;
+            while(j<m && content.charAt(i+j) == substr.charAt(j)) j++;
+            if(j == m) return i;
+        }
+        return NOT_FOUND;
     }
 
 }
