d4b106f9e48811910f61e46094ef77e5bf26d869
Filipe Ramos <42271005+fil1pe@users.noreply.github.com>
2018-10-30 10:53:09 -0300

    Create Utils.java

---
+++ string-search/src/main/java/pra/Utils.java
@@ -0,0 +1,16 @@
+package pra;
+
+public class Utils {
+    
+    /**
+     * A hash function for a substring str[begin...end-1]
+     */
+    public static hash(String str, int begin, int end){
+        int hash = 0;
+        for(int i=begin;i<end;i++){
+            // finish the function
+        }
+        return hash;
+    }
+    
+}
