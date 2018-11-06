5d87fee2f577045033853d66b73e6b01a2b4baad
Filipe Ramos <42271005+fil1pe@users.noreply.github.com>
2018-11-06 11:11:35 -0200

    Update Utils.java

--- string-search/src/main/java/pra/Utils.java
+++ string-search/src/main/java/pra/Utils.java
@@ -1,16 +1,44 @@
 package pra;
 
-public class Utils {
+/**
+ * A class for generating hash values
+ */
+public class Hash {
     
-    /**
-     * A hash function for a substring str[begin...end-1]
-     */
-    public static hash(String str, int begin, int end){
-        int hash = 0;
-        for(int i=begin;i<end;i++){
-            // finish the function
+    private final int R = 256;
+    private final int h;
+    private final int Q = 101; // A prime number
+    private final String str;
+    private int value = 0;
+    private int begin;
+    private final int wLength;
+    
+    public Hash(String str, int begin, int wLength){
+        this.str = str;
+        this.begin = begin;
+        this.wLength = wLength;
+        
+        int temp = 1;
+        for(int i=begin;i<end-1;i++) temp = (temp * R)%Q;
+        h = temp;
+        
+        for(int i=begin; i<begin + wLength; i++) value = (R*value + str[i])%Q;
+    }
+    
+    public void updateHash(){
+        begin++;
+        value = (R*(value - str[begin]*h) + str[begin+wLength])%Q;
+        if(value < 0) value += Q;
+    }
+    
+    @Override
+    public boolean equals(Object o){
+        if(!o instanceof Hash){
+            return false;
         }
-        return hash;
+          
+        if(this.value == ((Hash) o).value) return true;
+        return false;
     }
     
 }
