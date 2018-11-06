5025ca0c4fd5aa6a96568547e080e0d3066dbcd2
Filipe Ramos <42271005+fil1pe@users.noreply.github.com>
2018-11-06 10:35:40 -0200

    Update StringSearchStrategy.java

--- string-search/src/main/java/pra/stringsearch/StringSearchStrategy.java
+++ string-search/src/main/java/pra/stringsearch/StringSearchStrategy.java
@@ -7,9 +7,11 @@ public abstract class StringSearchStrategy {
 
     private final int NOT_FOUND = -1;
     private final String pattern;
+    private final int patternLength;
     
     public StringSearchStrategy(String pattern){
         this.pattern = pattern;
+        patternLength = pattern.length();
     }
 
     public int find(String content, int begin);
