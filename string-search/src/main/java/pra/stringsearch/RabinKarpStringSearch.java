a6a31f102c40aa2df5351a46ce55810c58e79416
Filipe Ramos <42271005+fil1pe@users.noreply.github.com>
2018-11-06 10:21:37 -0200

    Update RabinKarpStringSearch.java

--- string-search/src/main/java/pra/stringsearch/RabinKarpStringSearch.java
+++ string-search/src/main/java/pra/stringsearch/RabinKarpStringSearch.java
@@ -6,14 +6,18 @@ import static pra.Utils.hash;
  * Rabin Karp strategy for finding substring
  */
 public abstract class RabinKarpStringSearch extends StringSearchStrategy {
+    
+    public RabinKarpStringSearch(String pattern){
+        super(pattern);
+    }
 
-    public int find(String content, String substr, int begin){
+    public int find(String content, int begin){
         int n = content.length();
-        int m = substr.length();
-        int hpattern = hash(substr, 0, m);
+        int m = pattern.length();
+        int hpattern = hash(pattern, 0, m);
         for(int i=begin;i<n-m;i++){
             int hs = hash(content, i, i+m);
-            if(hs == hpattern) && substr.equals( content.substring(i, i+m) )) return i;
+            if(hs == hpattern) && pattern.equals( content.substring(i, i+m) )) return i;
         }
         return NOT_FOUND;
     }
