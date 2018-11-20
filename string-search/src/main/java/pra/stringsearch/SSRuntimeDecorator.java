a216240b5ef100c66a954b6523447edef5a9b5d6
Filipe Ramos <42271005+fil1pe@users.noreply.github.com>
2018-11-19 23:32:30 -0200

    Create SSRuntimeDecorator.java

---
+++ string-search/src/main/java/pra/stringsearch/SSRuntimeDecorator.java
@@ -0,0 +1,21 @@
+package pra.stringsearch;
+
+public class SSRuntimeDecorator extends StringSearchStrategy {
+    
+    private StringSearchStrategy strategy;
+    private String decoratedText;
+    
+    public SSRuntimeDecorator(StringSearchStrategy strategy, String decoratedText){
+        this.strategy = strategy;
+        this.decoratedText = decoratedText;
+    }
+    
+    public int find(String content, int begin){
+        long startTime = System.nanoTime();
+        int answer = strategy.find(content, begin);
+        long totalTime = System.nanoTime() - startTime;
+        decoratedText = totalTime + " ns";
+        return answer;
+    }
+    
+}
