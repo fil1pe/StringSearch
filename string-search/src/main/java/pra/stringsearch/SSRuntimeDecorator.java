5baf76d616f8a55dd7c5f019b391e0787e58bb0b
Filipe Ramos <42271005+fil1pe@users.noreply.github.com>
2018-11-19 23:47:58 -0200

    Update SSRuntimeDecorator.java

--- string-search/src/main/java/pra/stringsearch/SSRuntimeDecorator.java
+++ string-search/src/main/java/pra/stringsearch/SSRuntimeDecorator.java
@@ -3,9 +3,9 @@ package pra.stringsearch;
 public class SSRuntimeDecorator extends StringSearchStrategy {
     
     private StringSearchStrategy strategy;
-    private String decoratedText;
+    private StringBuilder decoratedText;
     
-    public SSRuntimeDecorator(StringSearchStrategy strategy, String decoratedText){
+    public SSRuntimeDecorator(StringSearchStrategy strategy, StringBuilder decoratedText){
         super("");
         this.strategy = strategy;
         this.decoratedText = decoratedText;
@@ -15,7 +15,7 @@ public class SSRuntimeDecorator extends StringSearchStrategy {
         long startTime = System.nanoTime();
         int answer = strategy.find(content, begin);
         long totalTime = System.nanoTime() - startTime;
-        decoratedText = totalTime + " ns";
+        decoratedText.append(totalTime + " ns");
         return answer;
     }
     
